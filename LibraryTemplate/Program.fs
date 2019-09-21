44dbf933c12332b7e0ec9dec0e10752e3583b81b
numnum <kazurego7@gmail.com>
2019-09-21 15:30:28 +0900

    Modレコードのメンバー追加と修正

--- LibraryTemplate/Program.fs
+++ LibraryTemplate/Program.fs
@@ -31,18 +31,44 @@ module AtCoderLibrary =
 
     module NumericFunctions =
         type Mod =
-            { divisor : int }
+            { divisor : int32 }
 
-            member this.Pow b n =
-                List.replicate b n
-                |> Seq.fold (fun accm i -> accm * i % int64 this.divisor) 1L
+            member this.Pow (b : int64) (n : int32) : int32 =
+                List.replicate n b
+                |> List.fold (fun product i -> product * i % int64 this.divisor)
+                       1L
                 |> int32
 
-            member this.Perm n k =
+            member this.Perm (n : int32) (k : int32) : int32 =
                 if k > n then 0
                 else
-                    seq { n - k + 1..n }
-                    |> Seq.fold (fun i m -> i * m % this.divisor) 1
+                    [ n - k + 1..n ]
+                    |> List.fold
+                           (fun product m ->
+                           product * int64 m % int64 this.divisor) 1L
+                    |> int32
+
+            member this.Fact(n : int32) : int32 = this.Perm n n
+
+            member this.FactTable(nMax : int32) =
+                [ 1..nMax ]
+                |> List.scan
+                       (fun product i -> product * int64 i % int64 this.divisor)
+                       1L
+                |> List.map int32
+                |> List.toArray
+
+            member this.CombTable (nMax : int32) (kMax : int32) =
+                let table = Array2D.zeroCreate nMax kMax
+                for n in interval 0 (nMax + 1) do
+                    for k in interval 0 (kMax + 1) do
+                        if n < k then table.[n, k] <- -1
+                        else if k = 0 then table.[n, k] <- 1
+                        else
+                            table.[n, k] <- int64 table.[n - 1, k - 1]
+                                            + int64 table.[n - 1, k]
+                                              % int64 this.divisor |> int32
+                table
 
 open AtCoderLibrary
 open InputOutputs
