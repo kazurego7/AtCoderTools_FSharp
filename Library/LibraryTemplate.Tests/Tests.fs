48661c937ccdc6320e66b17421d167f2b18189b4
numnum <kazurego7@gmail.com>
2019-09-22 20:34:46 +0900

    TestsのtestEqを省略せずtestEqualに変更

--- Library/LibraryTemplate.Tests/Tests.fs
+++ Library/LibraryTemplate.Tests/Tests.fs
@@ -3,25 +3,25 @@ module Tests
 open Expecto
 open AtCoder
 
-let testEq (actual : 'a) (expected : 'a) (message : string) : Test =
+let testEqual (actual : 'a) (expected : 'a) (message : string) : Test =
     test message { Expect.equal actual expected message }
 let testError (f : unit -> 'a) (message : string) : Test =
     test message { Expect.throws (fun () -> f() |> ignore) message }
 let mods = { NumericFunctions.Mod.divisor = 1_000_000_007 }
 let powTest =
-    testList "Pow" [ testEq (mods.Pow 17L 11) 896067736
+    testList "Pow" [ testEqual (mods.Pow 17L 11) 896067736
                          "n <> 0 && b <> 0 && over Int32.MaxValue"
-                     testEq (mods.Pow 0L 0) 1 "n = 0 && b = 0"
-                     testEq (mods.Pow 0L 41) 0 "n = 0"
-                     testEq (mods.Pow 151L 0) 1 "b = 0"
-                     testEq (mods.Pow 1L 1000) 1 "StackOverFlowかもしれない" ]
+                     testEqual (mods.Pow 0L 0) 1 "n = 0 && b = 0"
+                     testEqual (mods.Pow 0L 41) 0 "n = 0"
+                     testEqual (mods.Pow 151L 0) 1 "b = 0"
+                     testEqual (mods.Pow 1L 1000) 1 "StackOverFlowかもしれない" ]
 let permTest =
-    testList "Perm" [ testEq (mods.Perm 23 7) 235591273
+    testList "Perm" [ testEqual (mods.Perm 23 7) 235591273
                           "n <> 0 && k <> 0 && over Int32.MaxValue"
-                      testEq (mods.Perm 0 0) 1 "n = 0 && k = 0"
-                      testEq (mods.Perm 0 516) 0 "n = 0"
-                      testEq (mods.Perm 5 0) 1 "k = 0"
-                      testEq (mods.Perm 4 23) 0 "n < k"
+                      testEqual (mods.Perm 0 0) 1 "n = 0 && k = 0"
+                      testEqual (mods.Perm 0 516) 0 "n = 0"
+                      testEqual (mods.Perm 5 0) 1 "k = 0"
+                      testEqual (mods.Perm 4 23) 0 "n < k"
                       testError (fun () -> mods.Perm -7 31) "n < 0"
                       testError (fun () -> mods.Perm 13 -7) "k < 0" ]
 
