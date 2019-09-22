c40ddcd991a1e46e44e1edd4da0bc03ee7de1cd2
numnum <kazurego7@gmail.com>
2019-09-22 18:08:30 +0900

    Pow,Permのテストケースで、int32を超えていなかったのとメッセージを修正

--- Library/LibraryTemplate.Tests/Tests.fs
+++ Library/LibraryTemplate.Tests/Tests.fs
@@ -9,13 +9,15 @@ let testThrow (f : unit -> 'a) (message : string) : Test =
     test message { Expect.throws (fun () -> f() |> ignore) message }
 let mods = { NumericFunctions.Mod.divisor = 1_000_000_007 }
 let powTest =
-    testList "Pow" [ testEq (mods.Pow 13L 7) 62748517 "n <> 0 && k <> 0"
-                     testEq (mods.Pow 0L 0) 1 "n = 0 && k = 0"
+    testList "Pow" [ testEq (mods.Pow 17L 11) 896067736
+                         "n <> 0 && b <> 0 && over Int32.MaxValue"
+                     testEq (mods.Pow 0L 0) 1 "n = 0 && b = 0"
                      testEq (mods.Pow 0L 41) 0 "n = 0"
-                     testEq (mods.Pow 151L 0) 1 "k = 0"
+                     testEq (mods.Pow 151L 0) 1 "b = 0"
                      testEq (mods.Pow 1L 1000) 1 "StackOverFlowかもしれない" ]
 let permTest =
-    testList "Perm" [ testEq (mods.Perm 13 7) 8648640 "n <> 0 && k <> 0"
+    testList "Perm" [ testEq (mods.Perm 23 7) 235591273
+                          "n <> 0 && k <> 0 && over Int32.MaxValue"
                       testEq (mods.Perm 0 0) 1 "n = 0 && k = 0"
                       testEq (mods.Perm 0 516) 0 "n = 0"
                       testEq (mods.Perm 5 0) 1 "k = 0"
