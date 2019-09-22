69280ef6ca2d17e4aa2132932d086123bf41efc5
numnum <kazurego7@gmail.com>
2019-09-22 22:15:17 +0900

    テストコードの構成を少し変更

--- Library/LibraryTemplate.Tests/Tests.fs
+++ Library/LibraryTemplate.Tests/Tests.fs
@@ -7,23 +7,26 @@ let testEqual (actual : 'a) (expected : 'a) (message : string) : Test =
     test message { Expect.equal actual expected message }
 let testError (f : unit -> 'a) (message : string) : Test =
     test message { Expect.throws (fun () -> f() |> ignore) message }
-let mods = { NumericFunctions.Mod.divisor = 1_000_000_007 }
-let powTest =
-    testList "Pow" [ testEqual (mods.Pow 17L 11) 896067736
-                         "n <> 0 && b <> 0 && over Int32.MaxValue"
-                     testEqual (mods.Pow 0L 0) 1 "n = 0 && b = 0"
-                     testEqual (mods.Pow 0L 41) 0 "n = 0"
-                     testEqual (mods.Pow 151L 0) 1 "b = 0"
-                     testEqual (mods.Pow 1L 1000) 1 "StackOverFlowかもしれない" ]
-let permTest =
-    testList "Perm" [ testEqual (mods.Perm 23 7) 235591273
-                          "n <> 0 && k <> 0 && over Int32.MaxValue"
-                      testEqual (mods.Perm 0 0) 1 "n = 0 && k = 0"
-                      testEqual (mods.Perm 0 516) 0 "n = 0"
-                      testEqual (mods.Perm 5 0) 1 "k = 0"
-                      testEqual (mods.Perm 4 23) 0 "n < k"
-                      testError (fun () -> mods.Perm -7 31) "n < 0"
-                      testError (fun () -> mods.Perm 13 -7) "k < 0" ]
 
 [<Tests>]
-let tests = testList "NumericFunctions" [ powTest; permTest ]
+let tests =
+    let mods = { NumericFunctions.Mod.divisor = 1_000_000_007 }
+    let powTest =
+        testList "Mod.Pow"
+            [ testEqual (mods.Pow 17L 11) 896067736
+                  "n <> 0 && b <> 0 && over Int32.MaxValue"
+              testEqual (mods.Pow 0L 0) 1 "n = 0 && b = 0"
+              testEqual (mods.Pow 0L 41) 0 "n = 0"
+              testEqual (mods.Pow 151L 0) 1 "b = 0"
+              testEqual (mods.Pow 1L 1000) 1 "StackOverFlowかもしれない" ]
+    let permTest =
+        testList "Mod.Perm" [ testError (fun () -> mods.Perm -7 31) "n < 0"
+                              testError (fun () -> mods.Perm 13 -7) "k < 0"
+
+                              testEqual (mods.Perm 23 7) 235591273
+                                  "n <> 0 && k <> 0 && over Int32.MaxValue"
+                              testEqual (mods.Perm 0 0) 1 "n = 0 && k = 0"
+                              testEqual (mods.Perm 0 516) 0 "n = 0"
+                              testEqual (mods.Perm 5 0) 1 "k = 0"
+                              testEqual (mods.Perm 4 23) 0 "n < k" ]
+    testList "NumericFunctions" [ powTest; permTest ]
