f43820f59dbdf790f5099153e4e0830ef47369cc
numnum <kazurego7@gmail.com>
2019-09-22 20:32:01 +0900

    TestsのtestThrowをエラーであると強調したいのでtestErrorに変更

--- Library/LibraryTemplate.Tests/Tests.fs
+++ Library/LibraryTemplate.Tests/Tests.fs
@@ -5,7 +5,7 @@ open AtCoder
 
 let testEq (actual : 'a) (expected : 'a) (message : string) : Test =
     test message { Expect.equal actual expected message }
-let testThrow (f : unit -> 'a) (message : string) : Test =
+let testError (f : unit -> 'a) (message : string) : Test =
     test message { Expect.throws (fun () -> f() |> ignore) message }
 let mods = { NumericFunctions.Mod.divisor = 1_000_000_007 }
 let powTest =
@@ -22,8 +22,8 @@ let permTest =
                       testEq (mods.Perm 0 516) 0 "n = 0"
                       testEq (mods.Perm 5 0) 1 "k = 0"
                       testEq (mods.Perm 4 23) 0 "n < k"
-                      testThrow (fun () -> mods.Perm -7 31) "n < 0"
-                      testThrow (fun () -> mods.Perm 13 -7) "k < 0" ]
+                      testError (fun () -> mods.Perm -7 31) "n < 0"
+                      testError (fun () -> mods.Perm 13 -7) "k < 0" ]
 
 [<Tests>]
 let tests = testList "NumericFunctions" [ powTest; permTest ]
