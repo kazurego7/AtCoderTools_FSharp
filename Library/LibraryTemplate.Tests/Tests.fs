7d52aec13ba1eb55895f7dc9daa7d73e039cc41d
numnum <kazurego7@gmail.com>
2019-09-22 16:57:52 +0900

    Permのテスト追加 (k < 0 でおかしなことに)

--- Library/LibraryTemplate.Tests/Tests.fs
+++ Library/LibraryTemplate.Tests/Tests.fs
@@ -10,7 +10,12 @@ let powTest =
     testList "Pow" [ testEq (mods.Pow 0L 41) 0 "0^n = 0 (ただし n != 0)"
                      testEq (mods.Pow 151L 0) 1 "b^0 = 1"
                      testEq (mods.Pow 0L 0) 1 "0^0 = 0"
-                     testEq (mods.Pow 1L 1000) 1 "maybe StackOverFlow" ]
+                     testEq (mods.Pow 1L 1000) 1 "StackOverFlowかもしれない" ]
+let permTest =
+    testList "Perm" [ testEq (mods.Perm 5 0) 1 "k = 0"
+                      testEq (mods.Perm 4 23) 0 "n < k"
+                      testEq (mods.Perm 13 -7) 0 "k < 0"
+                      testEq (mods.Perm 0 0) 1 "n = k = 0" ]
 
 [<Tests>]
-let tests = testList "NumericFunctions" [ powTest ]
+let tests = testList "NumericFunctions" [ powTest; permTest ]
